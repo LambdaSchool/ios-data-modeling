19f22f52f4bf0b10019830c970be04d94a4dd2e1
Spencer Curtis <spencercolecurtis@gmail.com>
2019-09-24 10:46:07 -0600

    Add DataModeling playground with instructions

--- DataModeling.playground/Pages/Data Modeling - Model Creation.xcplaygroundpage/Contents.swift
+++ DataModeling.playground/Pages/Data Modeling - Model Creation.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
-
 import Foundation
+/*:
+ Assume we are making an app for a restaurant. Your job is to make a model object to represent a burrito and all of its various toppings.
 
-var str = "Hello, playground"
+ 1 - Create a struct called `Burrito`. Add a property for its name.
+ 
+ 2 - Create a second struct called `Topping` that has property for the topping's name and amount.
+ 
+ 3 - Back in the `Burrito` struct, create a variable called toppings whose type is an array of `Topping` objects.
+
+ **You will need these structs for the next assignment. Please copy them so you can paste them into the next assignment's code editor.**
+ */
+
+
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
