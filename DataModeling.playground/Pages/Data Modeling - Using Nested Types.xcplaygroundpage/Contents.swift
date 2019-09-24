343d41db9e88ed0403c0b17da71aa81d6f56ae4f
Spencer Curtis <spencercolecurtis@gmail.com>
2019-09-24 10:45:54 -0600

    Add .gitignore

---
+++ DataModeling.playground/Pages/Data Modeling - Using Nested Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ Continuing with the idea that we are making an app for a restaurant, let's assume we need to add a feature that "says" the name of every topping back to the user to make sure they are all what the user wants.
+
+
+ 1. Copy and paste the Burrito and Topping structs you made in the previous assignment if you haven't already.
+ 2. Make a function called readToppings(for burrito: Burrito). This function should loop through each topping of the burrito that was passed in to the function and print out the name of that topping.
+ 3. Initialize a Burrito in a constant and add a few toppings to it.
+ 4. Test your function by calling it and passing in the burrito you made in step 3. You should see each topping's name printed in the console.
+ */
+
+//: [Next](@next)
